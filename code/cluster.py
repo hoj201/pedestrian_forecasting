18adab7eb7e7cf1beb875fc0409dd05cd55c1579
hoj201 <hoj201@gmail.com>
2016-09-04 12:09:02 -0400

    made compatible with scene_class

--- code/cluster.py
+++ code/cluster.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def align_cluster( cluster ):
     """ returns cluster with curves aligned
 
@@ -73,21 +72,14 @@ def prune_cluster( cluster ):
     Q1 = lengths[n/4]
     Q3 = lengths[3*n/4]
     IQR = Q3-Q1
+
+    #Compute which to discard and count hoe many agents you discard
     keep_it = lambda traj: length_of_traj( traj ) < Q3+1.5*IQR and length_of_traj( traj ) > Q1-1.5*IQR
-    return filter( keep_it , cluster )
-
-def compute_prior( clusters ):
-    """ returns probability function over the clusters
-
-    args:
-        clusters ( list(list(numpy.ndarray)) ): each element is a numpy array of shape (2,N)
-
-    returns:
-        P_of_c numpy.ndarray: A 1D-array of positive floats which sum to 1.0
-    """
-    N_curves = reduce( lambda x,y: x+y, map( len, clusters ) )
-    P_of_c = [ len(c) / float(N_curves) for c in clusters]
-    return np.array( P_of_c )
+    bool2int = lambda b: 1 if b else 0
+    n_keep = reduce( lambda x,y:x+y, map( bool2int, map( keep_it, cluster) ) )
+    n_discarded = len(cluster) - n_keep
+    
+    return n_discarded, filter( keep_it , cluster )
 
 
 def merge_small_clusters( clusters):
@@ -99,16 +91,126 @@ def merge_small_clusters( clusters):
     returns:
         clusters (list(list(numpy.ndarray)) + [Null_ls]*N_0:
     """
-    new_clusters = [["Linear",] ,]
-    N_linear = 0
-    P_of_c = compute_prior( clusters )
-    for cl in clusters:
-        if len(cl) < 4 or P_of_c[k] < 0.1:
-            N_linear += len(cl)
+    new_clusters = []
+    n_discarded = 0
+    N_curves = reduce( lambda x,y: x+y, map( len, clusters ) )
+    portion = [ len(c) / float(N_curves) for c in clusters]
+    for k,cl in enumerate(clusters):
+        if len(cl) < 3 or portion[k] < 0.1:
+            n_discarded += len(cl)
         else:
             new_clusters.append( cl )
-    new_clusters[0] = ["Linear"]*N_linear
-    return new_clusters
+    return n_discarded, new_clusters
+
+
+def learn_potential( cluster , V_scale, k_max = 4, stride=30):
+    """Returns the legendre coefficients of a potential function learned from a list of point in a 2D domain.
+
+    args:
+        cluster (list(numpy.ndarray): list of trajectories.
+
+    kwargs:
+        stride (int): stride length to use along each trajectory.  Default=10
+
+    returns:
+        out (numpy.ndarray) : coeffs for 2D Legendre series describing potential funciton. shape = (k_max+1, k_max+1)
+    """
+    xy_arr = np.hstack( cluster )
+    x_arr = xy_arr[0][::stride]
+    y_arr = xy_arr[1][::stride]
+
+    # COST FUNCTION 
+    def cost_function(theta_flat ):
+        V_sum = 0
+        N = 0
+        theta = theta_flat.reshape( (k_max+1, k_max+1))
+        from numpy.polynomial.legendre import legval2d,leggauss
+        V_mean = legval2d( x_arr/V_scale[0], y_arr/V_scale[1], theta).mean()
+        k_span = np.arange( k_max+1)
+        res = 2*(k_max+10)
+        x_span = np.linspace(-V_scale[0], V_scale[0], res)
+        y_span = np.linspace(-V_scale[1], V_scale[1], res)
+        x_grid,y_grid = np.meshgrid(x_span, y_span)
+        Area = V_scale[0]*V_scale[1]*4
+        #TODO: Consider using scipy.dblquad to compute I
+        I = Area*np.exp( - legval2d( x_grid/V_scale[0], y_grid/V_scale[1] , theta)).mean()
+        regularization = np.sqrt( np.einsum( 'ij,i,j', theta**2 , k_span**2 , k_span**2 ) )
+        lambda_0 = 1e-4
+        return V_mean + np.log(I) + lambda_0 * regularization
+
+    # CONSTRAINTS
+    def potential_constraint(theta_flat ):
+        return theta_flat[0]
+    constraint_list = []
+    constraint_list.append({'type':'eq', 'fun':potential_constraint })
+    initial_guess = np.zeros( (k_max+1)**2 )
+
+    # CALLBACK FUNCTIONS
+    def cb_function( theta_flat ):
+        global k_max,curves
+        cb_function.iteration += 1
+        from progress_bar import update_progress
+        update_progress( cb_function.iteration / float(cb_function.max_iteration) )
+        return 0
+    cb_function.iteration = 0
+    cb_function.max_iteration = 1000
+
+    # MINIMIZE COST
+    from scipy.optimize import minimize
+    res = minimize( cost_function, initial_guess, constraints=constraint_list, callback = cb_function, options={'maxiter':cb_function.max_iteration})
+
+    # RETURN RESULT
+    print res.message
+    assert(res.success)
+    return res.x.reshape( (k_max+1, k_max+1) )
+
+
+def get_classes( curves, V_scale, k_max=4 ):
+    """ Given curves returns coefficients and probabilities and clusters
+
+    args:
+        clusters ( list(numpy.ndarray) ): a list of trajectories
+        V_scale : tuple of length 2.  Defines the size of the domain
+
+    kwargs:
+        k_max : degree of max polynomial to use in the negative likelihood field.
+
+    returns:
+        alpha, P_of_c, clusters_pruned
+
+
+    NOTE: alpha[k] and clusters_pruned[k] have probabilitiy P_of_c[k].  k=-1 corresponds to a linear predictor
+    """
+    #TODO: Test this.  Note, alpha[k] corresponds to P_of_c[k].  alpha[-1] corresponds to the linear predictor.
+    clusters = cluster_trajectories( curves )
+    
+    #Prune the clusters and keep track of how many agents you discard
+    n_discarded = 0
+    for k in range(len(clusters)):
+        n, clusters[k] = prune_cluster( clusters[k] )
+        n_discarded +=n
+    n, clusters = merge_small_clusters( clusters )
+    n_discarded += n
+
+    #print "There are %d nonlinear clusters" % len(clusters)
+    #from matplotlib import pyplot as plt
+    #fig, ax_arr = plt.subplots( len(clusters) )
+    #for k,cl in enumerate(clusters):
+    #    for curve in cl:
+    #        ax_arr[k].plot( curve[0], curve[1] )
+    #plt.show()
+
+    #Compute P_of_c
+    P_of_c = np.zeros( len(clusters) + 1 )
+    n_agents = n_discarded + reduce( lambda x,y: x+y, map( len , clusters ) )
+    P_of_c[0] = n_discarded / float(n_agents )
+    P_of_c[1:] = map( lambda c: len(c)/float(n_agents) , clusters)
+
+    #Compute alphas
+    alpha = np.zeros( (len(clusters)+1, k_max+1, k_max+1 ))
+    for k in range(len(clusters)):
+        alpha[k] = learn_potential( clusters[k] , V_scale, k_max=k_max)
+    return alpha, P_of_c, clusters
 
 if __name__ == "__main__":
     print "Testing clustering routine"
