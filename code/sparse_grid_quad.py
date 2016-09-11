cf2402ae2eee217ba58765172746034c8ea62191
hoj201 <hoj201@gmail.com>
2016-09-10 21:33:48 -0400

    added a script to output the number of nodes for each grid

--- code/sparse_grid_quad.py
+++ code/sparse_grid_quad.py
@@ -1,10 +1,20 @@
 import numpy as np
 from pandas import read_csv
-sparse_grid_nodes_3d = read_csv('./sparse_grid_data/points_3d.dat', sep=',', header=None).values
-sparse_grid_weights_3d = read_csv('./sparse_grid_data/weights_3d.dat', sep=',', header=None).values.flatten()
-sparse_grid_nodes_2d = read_csv('./sparse_grid_data/points_2d.dat', sep=',', header=None).values
-sparse_grid_weights_2d = read_csv('./sparse_grid_data/weights_2d.dat', sep=',', header=None).values.flatten()
+sparse_grid_nodes_3d = read_csv('./sparse_grid_data/points_3d.csv', sep=',', header=None).values
+sparse_grid_weights_3d = read_csv('./sparse_grid_data/weights_3d.csv', sep=',', header=None).values.flatten()
+sparse_grid_nodes_2d = read_csv('./sparse_grid_data/points_2d.csv', sep=',', header=None).values
+sparse_grid_weights_2d = read_csv('./sparse_grid_data/weights_2d.csv', sep=',', header=None).values.flatten()
+sparse_grid_nodes_1d = read_csv('./sparse_grid_data/points_1d.csv', sep=',', header=None).values.flatten()
+sparse_grid_weights_1d = read_csv('./sparse_grid_data/weights_1d.csv', sep=',', header=None).values.flatten()
 
+
+def sparse_grid_quad_1d( integrand, x_min, x_max ):
+    #Computes the sparse grid quadrature of the integrand over the interval [x_min,x_max]
+    transformed_nodes = np.zeros_like( sparse_grid_nodes_2d )
+    transformed_nodes[:,0] = sparse_grid_nodes_2d[:,0]*(x_max-x_min) + x_min
+    vals_on_nodes = integrand( transformed_nodes )
+    Vol = (x_max - x_min )
+    return np.dot( vals_on_nodes , sparse_grid_weights_2d ) * Vol
 
 def sparse_grid_quad_2d( integrand, x_min, x_max, y_min, y_max ):
     #Computes the sparse grid quadrature of the integrand over the box [x_min,x_max] x [y_min, y_max]
@@ -24,3 +34,12 @@ def sparse_grid_quad_3d( integrand, x_min, x_max, y_min, y_max, z_min, z_max ):
     vals_on_nodes = integrand( transformed_nodes.transpose() )
     Vol = ( x_max-x_min ) * ( y_max - y_min) * (z_max - z_min )
     return np.dot( vals_on_nodes , sparse_grid_weights_3d ) * Vol
+
+
+if __name__ == '__main__':
+    print "shape of 3d nodes array is "
+    print sparse_grid_nodes_3d.shape
+    print "shape of 2d nodes array is "
+    print sparse_grid_nodes_2d.shape
+    print "shape of 1d nodes array is "
+    print sparse_grid_nodes_1d.shape
