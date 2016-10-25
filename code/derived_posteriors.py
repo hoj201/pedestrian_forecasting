e96827c840f61616223c236a5a3ee3bc10f04406
hoj201 <hoj201@gmail.com>
2016-10-25 16:03:34 -0400

    added Owen's routines

---
+++ code/derived_posteriors.py
@@ -0,0 +1,245 @@
+from scene import Scene
+import posteriors
+import pickle
+import numpy as np
+from integrate import trap_quad
+from scipy.special import erf
+
+with open("data/test_scene.pkl", "rb") as f:
+    scene = pickle.load(f)
+max_k = len(scene.P_of_c)-1
+#temporary s_max
+s_max = 1
+s_max = scene.s_max
+#temporary dist_width
+dist_width = np.ones([2]) * scene.bbox_width
+vel_width = np.ones([2]) * scene.bbox_velocity_width
+sigma_v = scene.sigma_v
+p_of_lin = scene.P_of_c[-1]
+scene_scale = np.array([scene.width, scene.height])
+
+
+
+def unnorm_prob_k_s_x0(k, s, x0, x, v):
+    """
+    returns P(k,s,x0 | mu)
+    Takes:
+    k: float: class of agent
+    x0: np.array(2, N_points): points to be evaluated
+    x: np.array(2): given measurement of x0
+    v: np.array(2): given measurement of v0
+    s: np.array(N_points): initial speed
+    """
+    prob_measurement = posteriors.measurement_given_x0(x0, x)
+
+    xs = [x[0] for x in x0]
+    ys = [x[1] for x in x0]
+    xy = np.array([xs, ys])
+    initial_velocity = scene.director_field_vectorized(k, xy)
+    initial_velocity = np.dstack([s, s])[0] * np.dstack(initial_velocity)[0]
+    measurement_given_velocity = posteriors.measurement_given_v0(initial_velocity, v)
+    x0_given_k = posteriors.x0_given_k(k, x0)
+    p_k = scene.P_of_c[k]
+    p_s = posteriors.prob_s_uniform(s)
+    ret = prob_measurement * measurement_given_velocity * p_k * p_s * x0_given_k
+
+    return ret
+
+def _prob_lin_x_mu(x0, x, v):
+    """
+    returns P(lin,x|mu)
+    Takes:
+    x0: np.array(2, N_points): points to be evaluated
+    x: np.array(2): given measurement of x0
+    v: np.array(2): given measurement of v0
+    """
+
+    product = 1
+    product *= posteriors.measurement_given_x0(x0, x) * scene.P_of_c[-1] * posteriors.x0_given_lin(x0)
+    product /= 4 * sigma_v**2 * vel_width[0]**2 * scene_scale[0] * scene_scale[1]
+    term1 = erf((v[0] + vel_width[0]/2)/(np.sqrt(2) * sigma_v))
+    term2 = erf((v[0] - vel_width[0]/2)/(np.sqrt(2) * sigma_v))
+    product *= term1 - term2
+    term1 = erf((v[1] + vel_width[0]/2)/(np.sqrt(2) * sigma_v))
+    term2 = erf((v[1] - vel_width[0]/2)/(np.sqrt(2) * sigma_v))
+    product *= term1 - term2
+    return product
+
+def normalizing_constant(x,v):
+    bounds = (x[0] - dist_width[0]/2, x[0] + dist_width[0]/2,
+              x[1] - dist_width[0]/2, x[1] + dist_width[0]/2,
+              -1 * s_max, s_max)
+
+    normalizing_constant = 0
+    for i in range(max_k):
+        def temp(x1,y1,z):
+            x1 = x1.flatten()
+            y1 = y1.flatten()
+            #may be slow, possibly use dstack?
+            xy = np.array(zip(x1,y1))
+            z = z.flatten()
+            return unnorm_prob_k_s_x0(i, z, xy, x, v)
+        normalizing_constant += trap_quad(temp, bounds)
+
+    bounds = (x[0] - dist_width[0]/2, x[0] + dist_width[0]/2,
+              x[1] - dist_width[0]/2, x[1] + dist_width[0]/2)
+    def temp(x1,y1):
+        x1 = x1.flatten()
+        y1 = y1.flatten()
+        #may be slow, possibly use dstack?
+        xy = np.array(zip(x1,y1))
+        return _prob_lin_x_mu( xy, x, v)
+    normalizing_constant += trap_quad(temp, bounds)
+    return normalizing_constant
+
+#placeholder
+#meant to be summation over all k, s, x0
+#-smax to smax
+#all k
+#all x within bounding box of x0
+def prob_k_s_x0_given_mu(k, s, x0, x, v):
+    """
+    Takes
+    k: int: cluster
+    s: np.array(N_points), speeds of each initial point
+    x0: np.array(2, N_points), initial points to be evaluated
+    x: np.array(2): given position measurement
+    v: np.array(2): given velocity measurement
+    """
+
+    return 1.0/normalizing_constant(x, v) * unnorm_prob_k_s_x0(k, s, x0, x, v)
+
+def prob_lin_x_given_mu(x0, x, v):
+    """
+    Takes:
+    x0: np.array(2, N_points): Points to be evaluated
+    x: np.array(2): given position measuremnet
+    v: np.array(2): given velocity measurement
+    Returns: np.array(N_points), P(x0, lin|x, v)
+    """
+    return 1.0/normalizing_constant(x, v) * _prob_lin_x_mu(x0, x, v)
+
+def _prob_lin_x_v_mu(x0, v0, x, v):
+    """
+    Takes:
+    x0: np.array(2, N_points): positions to be evaluated
+    v0: np.array(2, n_points): velocities to be evaluated
+    x: np.array(2): position measurement
+    v: np.array(2): velocity measurement
+    returns np.array(N_points): P(x0, v0, lin, x, v)
+    """
+    product = 1
+    product *= posteriors.x0_given_lin(x0)
+    product *= posteriors.measurement_given_x0(x, x0)
+    product *= posteriors.measurement_given_v0(v, v0)
+    product *= posteriors.v0_given_x0_lin(v0)
+    product *= p_of_lin
+    return product
+
+def prob_lin_x_v_given_mu(x0, v0, x, v):
+    """
+    Takes:
+    x0: np.array(2, N_points): points to be evaluated
+    v0: np.array(2, N_points): velocities to be evaluated
+    x: np.array(2): measured position
+    v: np.array(2): measured velocity
+    Returns np.array(N_points): P(x0, v0, lin|x, v)
+    """
+    return _prob_lin_x_v_mu(x0, v0, x, v) / normalizing_constant(x, v)
+
+if __name__ == "__main__":
+    # print "starting k normalization"
+    # print scene_scale
+    # sum_k = 0
+    # print vel_width
+    # x = np.array([0.5, 0.5])
+    # v = np.array([0.011,011])
+    # bounds = (x[0] - dist_width[0]/2, x[0] + dist_width[0]/2,
+    #           x[1] - dist_width[0]/2, x[1] + dist_width[0]/2,
+    #           -1 * s_max, s_max)
+    # for i in range(max_k):
+    #     print i
+    #     def temp(x1,y1,z):
+    #         x1 = x1.flatten()
+    #         y1 = y1.flatten()
+    #         #may be slow, possibly use dstack?
+    #         xy = np.array(zip(x1,y1))
+    #         z = z.flatten()
+    #         return prob_k_s_x0_given_mu(i, z, xy, x, v)
+    #     sum_k += trap_quad(temp, bounds)
+    # print sum_k
+    # print "starting lin"
+    # bounds = (x[0] - dist_width[0]/2, x[0] + dist_width[0]/2,
+    #           x[1] - dist_width[0]/2, x[1] + dist_width[0]/2)
+    # def temp(x1,y1):
+    #     x1 = x1.flatten()
+    #     y1 = y1.flatten()
+    #     xy = np.array(zip(x1,y1))
+    #     return prob_lin_x_given_mu(xy, x, v)
+    # lin_term = trap_quad(temp, bounds)
+    # print lin_term
+    # sum_k += lin_term
+    # print sum_k
+
+    #sum_k = 0
+    #bounds = [-10*s_max, 10*s_max]
+    #x0 = np.array([[0,0]])
+    #x = np.array([0,0])
+    #v = np.array([0,0])
+    #print "starting second test"
+    #for i in range(max_k):
+    #    print i
+    #    def temp(z):
+    #        z = z.flatten()
+    #        x1 = np.zeros([len(z), 2])
+    #        return prob_k_s_x0_given_mu(i, z, x1, x, v)
+    #    sum_k += trap_quad(temp, bounds)
+    #print sum_k
+    #print posteriors.measurement_given_x0(x0, x)
+
+    #print prob_k_s_x0_given_mu(0, [0], x0, x, v)
+    #def temp(x, y):
+    #    x = x.flatten()
+    #    y = y.flatten()
+    #    #may be slow, possibly use dstack?
+    #    xy = np.array(zip(x,y))
+    #    return posteriors.measurement_given_v0(xy, np.array([0,0]))
+    #bounds = (-1 * vel_width[0], 1 * vel_width[0], -1 * vel_width[0], 1 * vel_width[0])
+    #print trap_quad(temp, bounds, res=(1000,1000))
+
+    #bounds = [-1 * s_max, s_max]
+    #print trap_quad(posteriors.prob_s_uniform, bounds)
+
+    #bounds = []
+    #for i in range(max_k):
+    #    def temp(x,y):
+    #        x = x.flatten()
+    #        y = y.flatten()
+    #        xy = np.array(zip(x,y))
+    #        return posteriors.x0_given_k(i, xy)
+    #    width = posteriors.scene_scale[0]
+    #    height = posteriors.scene_scale[1]
+    #    bounds = [-2*width/2.0,2* width/2.0, -2*height/2.0, 2*height/2.0]
+    #    print trap_quad(temp, bounds)
+
+    #bounds = [-.1, .1, -.1, .1]
+    #def temp(x,y):
+    #    x = x.flatten()
+    #    y = y.flatten()
+    #    xy = np.array(zip(x,y))
+    #    return posteriors.v0_given_x0_lin(xy)
+    #print trap_quad(temp, bounds)
+
+    k = 0
+    x0 = np.array([[0,0]])
+    x = np.array([0,0])
+    v = np.array([0,0])
+    s = np.array([.005])
+    dx = np.array([0,0.001])
+    #print unnorm_prob_k_s_x0(k, s, x0 + dx, x, v)
+    print unnorm_prob_k_s_x0(k, s, x0, x, v) / unnorm_prob_k_s_x0(k, s, x0 + dx, x, v)
+    print posteriors.x0_given_k(k, x0) / posteriors.x0_given_k(k, x0+dx)
+    pass
+
+
+
---
+++ code/posteriors.py
@@ -0,0 +1,136 @@
+import numpy as np
+#from energy import Vk
+from numpy.polynomial import legendre
+from integrate import trap_quad
+from scipy.stats import multivariate_normal
+import pickle
+from scene import Scene
+
+
+if __name__ == "__main__":
+    Vk = np.array([[[0, 0], [1,0]]])
+    scene_scale = np.ones([2])
+    dist_width = np.ones([2])
+    vel_width = np.ones([2])
+    s_max = 1
+else:
+    with open("data/test_scene.pkl", "rb") as f:
+        scene = pickle.load(f)
+    Vk = scene.alpha_arr
+    scene_scale = np.array([scene.width, scene.height])
+    #temporary
+    dist_width = np.ones([2]) * scene.bbox_width
+    vel_width = np.ones([2]) * scene.bbox_velocity_width
+    s_max = scene.s_max
+
+def legval_scale(x, y, coeffs):
+    """
+    Returns the values of a legend repolynomial
+    at N points, scaled so the whole plane is [0,1]**2
+
+    Takes x: np.array(N_Points): x values to evaluate
+    Takes y: np.array(N_Points): y values to evaluate
+    Takes coeffs: np.array(K_max_degree): legendre polynomial coefficients
+
+    Returns np.array(N_Points)
+
+    """
+    return legendre.legval2d(x/scene_scale[0], y/scene_scale[1], coeffs)
+
+
+
+def measurement_given_x0(x, x0):
+    """
+    Returns the probability at x given an x0
+
+    Takes x0: np.array(2): initial point
+    Takes x: np.array(2, N_Points): set of points at which to evaluate probability
+
+    Returns np.array(N_Points): probability that agent is at x given x0
+
+    """
+    #function to determine if points are within bounding box
+    filt = lambda y: 1 if (np.absolute(y[0] - x0[0]) <= dist_width[0]/2) and (np.absolute(y[1] - x0[1]) <= dist_width[0]/2) else 0
+    ident = np.array(map(filt, x))
+    return 1.0/( dist_width[0]**2) * ident
+
+
+def measurement_given_v0(v, v0):
+    """
+    Returns the probability at points v given a v0
+
+    Takes v0: np.array(2): initial point
+    Takes v: np.array(2, N_points): set of points at which to evaluate probability 
+
+    Returns np.array(N_Points): probability that agent is at v given v0
+    """
+    #filters out points not inside the bounding box.
+    filt = lambda y: 1 if (np.absolute(y[0] - v0[0]) <= vel_width[0]/2) and (np.absolute(y[1] - v0[1]) <= vel_width[0]/2) else 0
+    ident = np.array(map(filt, v))
+    return 1.0/(vel_width[0]**2) * ident
+
+def prob_s_uniform(s):
+    """
+    Returns P(s) assuming a uniform distribution of +-s_max
+    s_max: float
+    s: np.array(n_points): s values to be sampled
+    """
+    filt = lambda x: 1 if np.absolute(x) <= s_max else 0
+    ident = np.array(map(filt, s))
+    return 1.0/(2*s_max) * ident
+
+
+_normalization_constants = []
+_bounds = (-1 * scene_scale[0], scene_scale[0], -1 * scene_scale[1], scene_scale[1])
+
+for coeffs in Vk:
+    fn = lambda x, y: np.exp( -1*legval_scale(x, y, coeffs))
+    _normalization_constants.append(trap_quad(fn, _bounds))
+
+
+def x0_given_k(k, x0):
+    """
+    Returns probability that an agent will be at x0 given k
+
+    Takes k: int: cluster
+    Takes x0: np.array(2, N_points)
+
+    Returns np.array(N_points)
+    According to the equation (1/Z_K)exp(-1*V_k)
+    """
+    return (1.0/_normalization_constants[k])*np.exp(-1 * legval_scale(x0[:, 0], x0[:, 1], Vk[k]))
+
+def x0_given_lin(x0):
+    """
+    takes x0: np.array(2, N_points): points to be evaluated
+
+    Returns np.array(N_points)
+    """
+    return 1/(scene_scale[0] * scene_scale[1]) * np.ones(len(x0))
+
+def v0_given_x0_lin(v0):
+    """
+    Takes:
+    v0: np.array(2, N_points): points to be evaluated
+    Returns probability of v0 := N(0, sigma_v)
+    """
+    return multivariate_normal.pdf(v0, np.zeros([2]), scene.sigma_v)
+
+if __name__ == "__main__":
+    pass
+    #test measurement_given_x0, ensure it's constant
+
+    assert measurement_given_x0(np.array([[0,0]]), np.array([0,0]))[0] == measurement_given_x0(np.array([[0,0]]), np.array([0,0.5]))[0]
+
+    print "Expected answer: 0.07825"
+
+    print "Actual answer: "
+
+    print x0_given_k(0, np.array([[1, 0], [0,0], [1, 2]]))
+
+    print "testing x0_given_lin"
+    print x0_given_lin([[0,0],[0.5,0.5], [2,2]])
+
+    print measurement_given_v0([[0,0], [0.5, 0.5], [1,1], [2,2]], [0,0])
+
+
