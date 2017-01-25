dfc70ef82f83b13452209882c62a24116b69c593
Owen Hughes <okayhughes@gmail.com>
2017-01-25 16:07:38 -0500

    Delete a bunch, make probability of all classes equal

--- code/data.py
+++ code/data.py
@@ -6,7 +6,10 @@ for file in sorted(os.listdir("pickles")):
     if file.endswith("scene.pkl"):
         with open("pickles/" + file, "rb") as f:
             scene = pickle.load(f)
+            scene.P_of_c = np.ones_like(scene.P_of_c)/float(len(scene.P_of_c))
+            
             scenes.append(scene)
+
     elif file.endswith("set.pkl"):
         with open("pickles/" + file,'r') as f:
             sets.append(pickle.load(f))
