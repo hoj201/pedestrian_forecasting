805a968a111b0470308fda4caa1f6aebb51e447e
Owen Hughes <okayhughes@gmail.com>
2017-02-10 11:35:56 -0500

    Texy tex

--- code/kitani_over_time.py
+++ code/kitani_over_time.py
@@ -55,16 +55,21 @@ def fn(paths):
     #plt.savefig("images/kitani/AUC_vs_t_for_{}.png".format(name))
     #plt.show()
     return aucs, times
+ 
+from json_help import read_json
+order = read_json("scene_order.json")['order']
+params = read_json("params.json")
 
 rng = [0, 2, 3, 4]
-paths_kit = ["pickles/kitani/{}/".format(x) for x in rng]
-paths_ours = ["pickles/ours/{}/".format(x) for x in rng]
-paths_lin = ["pickles/linear/{}/".format(x) for x in rng]
-paths_rand = ["pickles/rand/{}/".format(x) for x in rng]
+sm = lambda x, y: x + y
+paths_kit = reduce(sm, [["pickles/kitani/{}/{}".format(x, j) for j in range(params['nfold'])] for x in rng])
+paths_ours = reduce(sm, [["pickles/ours/{}/{}".format(x, j) for j in range(params['nfold'])] for x in rng])
+paths_lin = reduce(sm, [["pickles/linear/{}/{}".format(x, j) for j in range(params['nfold'])] for x in rng])
+paths_rand = reduce(sm, [["pickles/rand/{}/{}".format(x, j) for j in range(params['nfold'])] for x in rng])
 
 our_auc, our_times = fn(paths_ours)
 kit_auc, kit_times = fn(paths_kit)
-lin_auc, lin_times = fn(paths_lin)
+#lin_auc, lin_times = fn(paths_lin)
 rand_auc, rand_times = fn(paths_rand)
 plt.clf()
 ax = plt.gca()
@@ -73,18 +78,62 @@ ax.set_xlabel("Frames")
 ax.set_ylabel("AUC")
 plt.plot(our_times, our_auc, ls="solid", c="black", label="Our Algorithm")
 plt.plot(kit_times, kit_auc, ls="dashed", c="black", label="Kitani Algorithm")
-plt.plot(lin_times, lin_auc, ls="dashdot", c="black", label="Linear Predictor")
-plt.plot(rand_times, rand_auc, ls="dotted", c="black", label="Random Walk")
+#plt.plot(lin_times, lin_auc, ls="dashdot", c="black", label="Linear Predictor")
+plt.plot(rand_times, rand_auc, ls="dashdot", c="black", label="Random Walk")
 plt.legend(loc="lower right")
 plt.savefig('images/The Results.eps', format='eps')
 plt.show()
 
+plt.clf()
+ax = plt.gca()
+
+def cold_blooded_murder(paths):
+    fs_tr = []
+    fs_pr = []
+
+    for path in paths:
+        for file in os.listdir(path):
+            if "AUC" in file or "DS" in file:
+                continue
+            undscrs = [i for i, ltr in enumerate(file) if ltr == "_"]
+            ag_num = int(file[undscrs[1]+1:undscrs[2]])
+            t = int(file[undscrs[3]+1:file.find(".")])
+            arr = np.load(path + file)
+            fs_pr.append((ag_num, t, arr)) if "pr" in file else fs_tr.append((ag_num, t, arr))
+
+    fs_tr_sorted = []
+    for i in fs_pr:
+        for j in ts_tr:
+            if j[0] == i[0] and j[1] == i[1]:
+                all_data.append(j)
+                continue
+    pr_arrs = [x[2] for x in fs_pr]
+    tr_arrs = [x[2] for x in fs_tr_sorted]
+
+    sm = lambda x, y: np.concatenate((x, y))
+    comp_pr = reduce(sm, pr_arrs)
+    comp_tr = reduce(sm, tr_arrs)
+    fpr, tpr, thr = roc_curve(trs, prs)
+    return fpr, tpr
+
+fpr_ours, tpr_ours = cold_blooded_murder(paths_ours)
+fpr_kit, tpr_kit = cold_blooded_murder(paths_kit)
+#lin_auc, lin_times = fn(paths_lin)
+fpr_rand, tpr_rand = cold_blooded_murder(paths_rand)
 
 
 
-
-
-
+ax.set_ylim([-.1, 1.1])
+ax.set_xlim([-.1, 1.1])
+ax.set_xlabel("FPR")
+ax.set_ylabel("TPR")
+plt.plot(fpr_ours, tpr_ours, ls="solid", c="black", label="Our Algorithm")
+plt.plot(fpr_kit, tpr_kit, ls="dashed", c="black", label="Kitani Algorithm")
+#plt.plot(lin_times, lin_auc, ls="dashdot", c="black", label="Linear Predictor")
+plt.plot(fpr_rand, tpr_rand, ls="dashdot", c="black", label="Random Walk")
+plt.legend(loc="lower right")
+plt.savefig('images/The ROC Results.eps', format='eps')
+plt.show()
 
 
 
